8f5a7bbd7471f4210bfd2c94e32a416fdb356f08
RisingOrange <jakub.fidler@protonmail.com>
2026-04-19 22:56:34 +0200

    Expand smoke to 9 high-value routes

--- tests/visual/smoke.spec.ts
+++ tests/visual/smoke.spec.ts
@@ -1,6 +1,20 @@
 import { test } from '@chromatic-com/playwright'
 
-test('homepage', async ({ page }) => {
-	await page.goto('/')
-	await page.evaluate(() => document.fonts.ready)
-})
+const ROUTES = [
+	'/',
+	'/outcomes',
+	'/funding',
+	'/donate',
+	'/action',
+	'/learn',
+	'/about',
+	'/communities',
+	'/press'
+]
+
+for (const path of ROUTES) {
+	test(`visit ${path}`, async ({ page }) => {
+		await page.goto(path)
+		await page.evaluate(() => document.fonts.ready)
+	})
+}
